957bc653c979600b38c2939c2fdc63db84a9238b
Pierre Rubens Milorme <prubensmilorme@gmail.com>
2025-03-17 01:26:55 -0400

    las

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -153,52 +153,35 @@
             @endif
         </div>
 
-        <a href="{{ route('liens-utiles') }}" class="block text-gray-700 hover:text-orange-500 p-2">
+        <a href="{{ route('liens-utiles') }}" class="block text-gray-700 hover:text-orange-500 p-2 pl-0">
             {{ __('messages.useful_links') }}
         </a>
-        <a href="#" class="block text-gray-700 hover:text-orange-500 p-2">
+        <a href="" class="block text-gray-700 hover:text-orange-500 p-2 pl-0">
             {{ __('messages.contact') }}
         </a>
+
         @guest
-            <a href="{{ route('login') }}" class="block text-gray-700 hover:text-orange-500 p-2">
+            <a href="{{ route('login') }}" class="block text-gray-700 hover:text-orange-500 p-2 pl-0">
                 {{ __('messages.login') }}
             </a>
         @endguest
+
         @auth
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button
-                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-[#173152] hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                onclick="event.preventDefault();
-                                            this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
+            <!-- Mobile-friendly user menu -->
+            <div class="space-y-2">
+                <div class="px-2 py-3 text-orange-500 font-medium pl-0 underline">
+                    {{ Auth::user()->name }}
+                </div>
+                {{--                 <a href="{{ route('profile.edit') }}" class="block text-gray-700 hover:text-orange-500 p-2">
+                    {{ __('Profile') }}
+                </a> --}}
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a href="{{ route('logout') }}" class="block text-gray-700 hover:text-orange-500 p-2"
+                        onclick="event.preventDefault(); this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </a>
+                </form>
             </div>
         @endauth
     </div>
